c1c97108a2e00d3ee9bbb2ae106b508e0d0a03d4
sunriseL <540729251@qq.com>
2021-11-18 20:00:57 +0800

    add automatic filter

--- compact.py
+++ compact.py
@@ -2,6 +2,7 @@
 import csv
 import os
 import deploy
+import functools
 data_header = ['No.', 'Team', 'Artist', 'Genre', 'Title', 'Impr', 'Total', 'Median', 'Regist', 'Update']
 target_type = ['name','type','value','date']
 
@@ -13,6 +14,66 @@ def turn_data_into_target(data,date):
     result.append(date)
     return result
 
+def bofxvi_compare(x,y):
+    x = x[7:-4]
+    y = y[7:-4]
+    m_x = x[0:2]
+    m_y = y[0:2]
+    if m_x == "01": m_x = "13"
+    if m_y == "01": m_y = "13"
+
+    d_x = x[3:5]
+    d_y = y[3:5]
+    h_x = x[6:8]
+    h_y = y[6:8]
+    if m_x > m_y: return 1
+    elif m_x < m_y: return -1
+
+    if d_x > d_y: return 1
+    elif d_x < d_y: return -1
+
+    if h_x > h_y: return 1
+    elif h_x < h_y: return -1
+
+    return 0
+
+
+def filter_log_list(dirpath, names):
+    new_names = []
+    datas = []
+    for index, name in enumerate(names):
+        with open(os.path.join(dirpath,name),encoding="utf-8") as f:
+            time_slice = name[7:-4]
+            # end = time_slice == "12-20-23"
+            # final_end = time_slice == "01-05-20"
+            # last = (time_slice > "12-20-23" or time_slice.startswith("01")) and not final_end
+            r = csv.reader(f)
+            header = next(r)
+            rows = [row for row in r]
+            rows.sort(key=lambda row: eval(row[data_header.index("Total")]),reverse=True)
+            rows = rows[:20]
+            datas.append(rows)
+
+            ''' skip log which does not have data '''
+            if eval(rows[0][data_header.index("Total")]) == 0:
+                continue
+
+            '''  '''
+            if index != 0:
+                titles = [row[data_header.index("Title")] for row in rows]
+                prev_titles = [row[data_header.index("Title")] for row in datas[index-1]]
+                s = 0
+                for prev_row, row in zip(datas[index-1], rows):
+                    s += eval(row[data_header.index("Total")]) - eval(prev_row[data_header.index("Total")])
+                # if (s > 8000 or s < -8000 or end or final_end) and not last:
+                if s > 8000 or s < -8000:
+                    print(time_slice)
+                    new_names.append(name)
+            else:
+                new_names.append(name)
+            #print(titles)
+    return new_names
+
 def compact(Event="BOFXV"):
     print("Compacting {0}...".format(Event))
     names = []
@@ -22,7 +83,9 @@ def compact(Event="BOFXV"):
 		    for fn in filenames:
 			    if fn.startswith(Event):
 				    names.append(fn)
-    names.sort()
+    dirpath = 'data'
+    names.sort(key=functools.cmp_to_key(bofxvi_compare))
+    names = filter_log_list(dirpath, names)
     for name in names:
         times = name[:name.index(".")].split("-")[-3:]
         date = "{0}/{1} {2}:00".format(times[0],times[1],times[2])
@@ -36,7 +99,7 @@ def compact(Event="BOFXV"):
     print("Compact Over")
 
 def compact_with_list(names):
-    names.sort()
+    names.sort(key=functools.cmp_to_key(bofxvi_compare))
     datas = []
     dirpath="data"
     for name in names:
